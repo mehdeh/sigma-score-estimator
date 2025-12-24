ce96b8590c81a9b4c5eb7b078e5dfc29f31ad2bb
mehdi <mehdi2774787@gmail.com>
2025-12-24 13:46:38 +0000

    chore(tests): remove test directory
    - Remove test files and test suite

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Unit tests for sigma-score-estimator.
-
-This package contains unit tests for:
-- Output transformations (test_output_transforms.py)
-- Loss functions (test_loss_functions.py)
-- Evaluator integration (test_evaluator_integration.py)
-"""
-
